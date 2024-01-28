d52dde5c4ad19f591df82ceb60a1c02a5f1e8c2b
= <alfred@stensatter.se>
2024-01-28 19:32:30 +0100

    asd

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main(int argv, char **argc) {
 
     puts(HIDE_CURSOR);
 
-    for(int i = 1; i < 200; i +=1){
+    for(int i = 1; i < 500; i +=1){
         char str[25];
         if(i > 999)
             sprintf(str,"./%s/%d.png",path,i);
@@ -38,7 +38,7 @@ int main(int argv, char **argc) {
         else
             print_frame(new_frame);
 
-        msleep(33);
+        msleep(0);
     }
     free_frame(new_frame);
     free_frame(prev_frame);
